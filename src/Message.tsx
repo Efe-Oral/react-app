0bd78cc6201ca2fc461dc7fdca97d3463714047b
efeoral <efeoral@gmail.com>
2024-10-31 19:12:12 +0100

    dynamic name added

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+
+function Message()
+{
+    const name = "Sevi";
+    if (name)
+        return <h1> Hello {name}, I'm back! </h1>;
+    return <h1>Bozacı Mevlut </h1>;
+}
+
+export default Message;
