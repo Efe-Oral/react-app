fd39a4b8b9fdca7eb25b178de8a6efa5baf2cabb
efeoral <efeoral@gmail.com>
2024-10-31 20:01:26 +0100

    checking item exists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,31 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
+  let items = ["Berlin", "Dresden", "Würzburg"];
+  items = [];
+
+  const getMessage = () => {
+    return items.length === 0 ? <p> No items found </p> : null;
+  };
+  /* Alternative way of cheking "No items found"
+  if (items.length === 0)
+    return (
+      <Fragment>
+        <h1>
+          <p>No items found </p>
+        </h1>
+      </Fragment>
+    );
+  */
+
   return (
     <Fragment>
       <h1> List </h1>
+      {items.length === 0 ? <p> No items found </p> : null} //getMessage()
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}> {item} </li>
+        ))}
       </ul>
     </Fragment>
   );
