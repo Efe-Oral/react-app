bbf56395c2919676beef8173b5be6f56e3880c55
efeoral <efeoral@gmail.com>
2024-10-31 19:35:32 +0100

    List group added

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,11 +1,7 @@
-
-
-function Message()
-{
-    const name = "Sevi";
-    if (name)
-        return <h1> Hello {name}, I'm back! </h1>;
-    return <h1>Bozacı Mevlut </h1>;
+function Message() {
+  const name = "Sevi";
+  if (name) return <h1> Hello {name}, I'm back! </h1>;
+  return <h1>Bozacı Mevlut </h1>;
 }
 
 export default Message;
